430551bff3ba21633623da800fc4161836a12622
Biraj <biraj.pub@gmail.com>
2026-07-17 15:39:06 -0700

    fix: update CORS middleware to log allowed origins and adjust header handling

--- shared/middleware/cors.ts
+++ shared/middleware/cors.ts
@@ -3,15 +3,16 @@ import type { NextFunction, Request, Response } from "express";
 import { sharedEnv } from "../env";
 
 const allowedOrigins = new Set(sharedEnv.CORS_ORIGIN);
+console.log("CORS_ORIGIN:", Array.from(allowedOrigins).join(", "));
 const allowAll = allowedOrigins.has("*");
 const fallbackOrigin = sharedEnv.CORS_ORIGIN[0];
 
 export default function cors(req: Request, res: Response, next: NextFunction) {
   const requestOrigin = req.headers.origin;
 
-  if (allowAll) {
-    res.header("Access-Control-Allow-Origin", "*");
-  } else if (requestOrigin && allowedOrigins.has(requestOrigin)) {
+  // we cannot send "*" as the Access-Control-Allow-Origin header if credentials are allowed
+  // (which are) in the frontend, so we must echo back the request origin if it's allowed
+  if (allowAll || (requestOrigin && allowedOrigins.has(requestOrigin))) {
     res.header("Access-Control-Allow-Origin", requestOrigin);
   } else if (sharedEnv.NODE_ENV === "dev" && requestOrigin) {
     res.header("Access-Control-Allow-Origin", requestOrigin);
